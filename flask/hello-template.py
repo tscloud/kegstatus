d0644a0a0dd88621a4ce9bec2c52a8e01942bb74
tscloud <thomas.s.cloud@gmail.com>
2017-10-20 12:48:37 -0500

    flask template script tweaks

--- flask/hello-template.py
+++ flask/hello-template.py
@@ -9,23 +9,17 @@ app = Flask(__name__)
 sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
 
 @app.route("/")
-def hello():
+def getSensorData():
 	now = datetime.datetime.now()
 	timeString = now.strftime("%Y-%m-%d %H:%M")
-	templateData = {
-		'title' : 'HELLO!',
-		'time': timeString
-	}
-	return render_template('main.html', **templateData)
 
-@app.route("/sensor")
-def getSensorData():
 	temp = sensor.read_temperature_f()
 	humidity = sensor.read_humidity()
 	pressure = sensor.read_pressure() / 100
 	
 	templateData = {
 		'title' : 'SENSOR!',
+		'time' : timeString,
 		'temp' : temp,
 		'humidity': humidity,
 		'pressure': pressure
@@ -33,4 +27,4 @@ def getSensorData():
 	return render_template('sensor.html', **templateData)
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=8080, debug=True)
+	app.run(host='0.0.0.0', port=8088, debug=True)
